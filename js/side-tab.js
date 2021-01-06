a92ef0ac8a3770ccc0c365cee9a45430db4ab508
chinsun9 <40482203+chinsun9@users.noreply.github.com>
2021-01-06 17:58:05 +0900

    Refactor code formatting
    1. css property order sort
    2. display: flex -> grid
    3. delete empty line

--- js/side-tab.js
+++ js/side-tab.js
@@ -1,10 +1,8 @@
 // set-repo-link
-
-
 const setRepoLink = () => {
     const side_tab = document.querySelector(".sidetab a")
 
-    const repoURL = `https://github.com/chinsun9${location.pathname}` 
+    const repoURL = `https://github.com/chinsun9${location.pathname}`
     side_tab.href = repoURL
 }
 
