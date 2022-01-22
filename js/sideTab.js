fa06777079fce999935a38d75265d8fc53de81b7
chinsun9 <chin_sung@naver.com>
2022-01-22 21:33:23 +0900

    refactor: remove sideTab.js

--- js/sideTab.js
+++
@@ -1,9 +0,0 @@
-// set-repo-link
-const setRepoLink = () => {
-    const sideTab = document.querySelector(".sidetab a")
-
-    const repoURL = `https://github.com/chinsun9${location.pathname}`
-    sideTab.href = repoURL
-}
-
-setRepoLink()
